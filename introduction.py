1c60827a95f29258fe9f9642d0489b53ce1bf44f
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-22 22:43:05 +0000

    Class Index and Slicing

--- introduction.py
+++ introduction.py
@@ -89,4 +89,21 @@ number_string = str(age)
 print("Tu edad es: " + number_string)
 """
 
+""""
 #Class Index and Slicing
+saludo = "Hola"
+print(saludo[0])
+print(saludo[2])
+
+#Negative Index
+print(saludo[-1])
+
+#Slicing
+palabra = "Python"
+print(palabra[0:3])
+print(palabra[2:4])
+
+#Longitud de una cadena
+cadena_larga = "Mi nombre es Diana Aviles"
+print(len(cadena_larga))
+"""
