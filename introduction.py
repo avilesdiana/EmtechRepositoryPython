7c42e15f15f8f440757a5f7ba57202ff7b56403b
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-22 21:33:06 +0000

    Class Numbers

--- introduction.py
+++ introduction.py
@@ -1,7 +1,9 @@
+"""
 print("My first code in Repl")
 
 #Class Variables
 #Declaring Variables
+
 edad = 23  #name->edad, value->23
 altura = 1.7
 institucion = "Emtech"  #name->institucion, value->Emtech
@@ -18,3 +20,28 @@ print(nombre_amigo1)
 
 #Print the value directly
 print("Diana Aviles")
+"""
+
+
+#Class Numbers
+
+suma = 6 + 5
+resta = 6 + 5
+multiplicacion = 6 * 5
+division = 6 / 5
+
+print (suma)
+print (resta)
+print (multiplicacion)
+print (division)
+
+operaciones_matematicas = 6 + 3 - 1 * 3 / 2
+print(operaciones_matematicas)
+
+#in division there is always a decimal part
+division_flotante = 15 / 3
+print(division_flotante)
+
+#Convert to int
+division_entera = 15 // 3
+print(division_entera) #In python there is no rounding
