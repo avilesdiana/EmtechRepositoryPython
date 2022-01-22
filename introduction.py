bf026191d475c7a2bbfd1f041c10868751fbf09d
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-22 21:58:31 +0000

    Class Text

--- introduction.py
+++ introduction.py
@@ -22,7 +22,7 @@ print(nombre_amigo1)
 print("Diana Aviles")
 """
 
-
+""""
 #Class Numbers
 
 suma = 6 + 5
@@ -44,4 +44,39 @@ print(division_flotante)
 
 #Convert to int
 division_entera = 15 // 3
-print(division_entera) #In python there is no rounding
+print(division_entera) #In python there is no rounding 
+
+division_entera1 = 29 // 3
+print(division_entera1) #print 9 to 9.666
+
+"""
+
+#Class Textos
+
+mi_cadena = "Hola mundo"
+mi_cadena2 = 'Hola mundo'
+
+print(mi_cadena)
+
+cadena_con_comillas = 'Diana dijo: "Hola mundo"'
+print(cadena_con_comillas)
+
+#Other form to strings with quotation marks
+cadena_con_comillas2 = "Diana dijo: \"Hola mundo 2\""
+print(cadena_con_comillas2)
+
+#Multilinea
+multilinea = """Hola, bienvenida
+
+A tu curso de python
+"""
+print(multilinea)
+
+#Unión de cadenas
+union_cadenas = multilinea + cadena_con_comillas
+print(union_cadenas)
+
+#Convert to string
+age = 23
+number_string = str(age)
+print("Tu edad es: " + number_string)
