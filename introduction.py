1e530a154ebc1ccfc74b8b93398e919cde16bc16
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-23 15:45:25 +0000

    Class Booleanos y Comparaciones

--- introduction.py
+++ introduction.py
@@ -155,4 +155,18 @@ valor_float = float(input("Ingresa un número flotante: "))
 print( valor_float * 7 )
 """
 
+"""
 #Booleanos y Comparaciones
+productos = 21
+caja_rapida = productos <= 10 
+caja_autocobro = productos < 26
+
+print(caja_rapida)
+print(caja_autocobro)
+
+#example
+numero_ganador = 23
+numero_cliente = int(input('Ingresa un número para adivinar: '))
+
+print(numero_ganador == numero_cliente)
+"""
