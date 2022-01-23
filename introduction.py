2baf4968d33b49d9de069343da164c6258aee875
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-23 18:23:51 +0000

    Operadores de Asignación

--- introduction.py
+++ introduction.py
@@ -182,3 +182,20 @@ productos = ["Cereal", "Pan", "Leche"]
 print(productos[2] == "Jugo")
 print(prductos[1] == "Pan")
 """
+
+"""
+#And & - or |
+edad_cliente = int(input('Ingresa tu edad: '))
+tarjeta_credito = edad_cliente >= 18 and edad_cliente <= 70
+print('Puedes tener tarjeta de credito: ', tarjeta_credito)
+
+descuento = edad_cliente == 20 or edad_cliente == 30
+print('Tiene el descuento: ',descuento)
+
+cadena = input('Ingresa una cadena: ')
+resultado_cadena = len(cadena) > 5 and cadena[2]== "h"
+print("Resultado cadena: ", resultado_cadena)
+"""
+
+
+#Class Operadores de Asignación
