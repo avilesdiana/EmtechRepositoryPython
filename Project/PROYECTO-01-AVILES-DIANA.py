81d76c4385786975ce6bb6ca4aae02b28738e4d0
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-26 19:09:20 +0000

    Estructura del Proyecto

--- Project/PROYECTO-01-AVILES-DIANA.py
+++ Project/PROYECTO-01-AVILES-DIANA.py
@@ -1,40 +1,138 @@
-import os
-import getpass
+import os #Clase para limpiar pantalla
+import getpass #Clase para que la contraseña no aparezca en consola
+import time
 
-
+#Función para limpiar pantalla
 def clearConsole():
     command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+    if os.name in ('nt', 'dos'):  #Si el programa corre en Windows, use cls
         command = 'cls'
     os.system(command)
 
+#Función para mostrar mensaje: Presionar Enter y contniuar
 def enter():
-  input('\n[Presiona Enter para continuar]')
+  input('\n[Presiona Enter para continuar] ')
 
-#Count
+#Función para mostrar mensje Bienvenida
+def welcome_System():
+  print('\nBienvenido a LIFESTORE')
+  print('\nIngresa tus credenciales de administrador para acceder a los reportes\n')
+
+#Función para mostrar el menú principal 
+def main_menu():
+  print('\nMENU LIFESTORE\n-------------------------------')
+  print('\nSeleccione una opción\n')
+  print('\n1.Productos más vendidos y productos rezagados')
+  print('\n2.Productos por reseña en el servicio')
+  print('\n3.Total de ingresos y ventas promedio mensuales,\ntotal anual y meses con meses con más ventas en el año')
+  print('\n4.Cerrar sesión\n')
+  choiceOfMainMenu = input('\nNúmero de elección: ')
+  return choiceOfMainMenu #Regresa la elección para su validación en el programa principal
+
+#Función para mostrar el cerrado de sesión
+def log_Out():
+  clearConsole()
+  print('Cerrando sesión de LIFESTORE .')
+  time.sleep(1)
+  clearConsole()
+  print('Cerrando sesión de LIFESTORE ..')
+  time.sleep(1)
+  clearConsole()
+  print('Cerrando sesión de LIFESTORE ...')
+  time.sleep(1)
+  clearConsole()
+  print('Cerrando sesión de LIFESTORE ....')
+  time.sleep(1)
+  clearConsole()
+
+#Función para mostrar que ingreso un valor erróneo 
+def wrong_Value(optionText):
+  print('\n    Opción INCORRECTA')
+  print('\nIngrese un valor de '+ optionText+' válido')
+
+#Inicializando el contador para la evaluación
 validation_Count = 0
+validation_Menu = 0
+session_error = 3
+
 #Login
 user_Login = 'admin'
 password_Login = '1234'
 
-
-while validation_Count == 0:
+#Estructura del programa
+#Mientras el contador == 0, repetira la acción hasta que ingrese el valor correcto
+while validation_Count == 0 and session_error != 0:
+  #Mensaje de bienvenida
+  clearConsole()
+  welcome_System() 
   user_Access = input('USUARIO: ')
+
   if user_Access == user_Login:
+    #getpass es para que no aparezca la contraseña en la consola
     password_Access = getpass.getpass('CONTRASEÑA: ')
+
     if password_Access == password_Login:
+      clearConsole() #Limpiar consola
       print('\nACCESO OTORGADO')
-      break
+      #Al ser diferente de 0 sale del ciclo while
+      validation_Count += 1
+      clearConsole()
+      
+      #validación del menú, mientras el contador == 0 lo seguirá repitiendo
+      while validation_Menu == 0:
+        choiceMainMenu = int(main_menu())
+      
+        if choiceMainMenu == 1:
+          clearConsole()
+          print('Elección 1')
+          enter()
+          validation_Menu += 1
+
+        elif choiceMainMenu == 2:
+          clearConsole()
+          print('Elección 2')
+          enter()
+          validation_Menu += 1
+      
+        elif choiceMainMenu == 3:
+          clearConsole()
+          print('Elección 3')
+          enter()
+          continue
+          validation_Menu += 1
+        
+        elif choiceMainMenu == 4:
+          #función para cerrar sesión
+          log_Out()
+          validation_Menu += 1
+        
+        else:
+         #Ingreso una valor erróneo en el menú
+         clearConsole()
+         wrong_Value('menú')
+         enter()
+         clearConsole() 
+
+    #else contraseña
     else:
+      #Ingreso una contraseña errónea
       clearConsole()
-      print('\nCONTRASEÑA INCORRECTA')
+      wrong_Value('contraseña')
       enter()
-      validation_Count == 1
       clearConsole()
+      session_error -=1
+      print("PASASTE LAS 3 VECES,\nContacta a tu gerente para revisar tus credenciales")
+      
+
+  #else usuario   
   else:
-    print('\nUSUARIO INCORRECTO')
-    enter()
-    validation_Count == 1
+    #Ingreso un Usuario erróneo
     clearConsole()
+    wrong_Value('usuario')
+    enter()
+    clearConsole()
+    session_error -=1
+    print("PASASTE LAS 3 VECES,\ncontacta a tu gerente para revisar tus credenciales")
+    
 
     
