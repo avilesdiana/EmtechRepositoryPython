130a3f44a9500398f882ae9e092639402309ae8d
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-01-26 15:32:38 +0000

    Class Break Continue

--- flow_Control/break_continue.py
+++ flow_Control/break_continue.py
@@ -1,1 +1,46 @@
 #Class Break Continue
+
+#Detener línea de producción  en cuanto lea defectuoso
+autos = ['correcto', 'correcto', 'correcto', 'defectuoso', 'correcto','defectuoso', 'correcto']
+
+for estado in autos:
+  if estado == 'defectuoso':
+    print('AUTO DEFECTUOSO REVISAR, deteniendo linea de producción')
+    break
+
+  print('Este auto esta ' + estado)
+
+"""
+Este auto esta correcto
+Este auto esta correcto
+Este auto esta correcto
+AUTO DEFECTUOSO REVISAR
+"""
+print('-------------------------------')
+autos1 = ['correcto', 'correcto', 'correcto', 'defectuoso', 'correcto','defectuoso', 'correcto']
+
+for estado1 in autos1:
+  if estado1 == 'defectuoso':
+    print('AUTO DEFECTUOSO, enviado a revisión')
+    continue
+
+  print('Este auto esta ' + estado1)
+  print('Enviado para distribución')
+
+"""
+
+Este auto esta correcto
+Enviado para distribución
+Este auto esta correcto
+Enviado para distribución
+Este auto esta correcto
+Enviado para distribución
+AUTO DEFECTUOSO, enviado a revisión
+Este auto esta correcto
+Enviado para distribución
+AUTO DEFECTUOSO, enviado a revisión
+Este auto esta correcto
+Enviado para distribución
+
+"""
+
