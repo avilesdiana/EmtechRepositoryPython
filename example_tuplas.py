d3626dcab15916cf2a4d2016eb67fff837d1949b
Diana Aviles <36268457+avilesdiana@users.noreply.github.com>
2022-02-12 19:54:19 +0000

    Tuplas

---
+++ example_tuplas.py
@@ -0,0 +1,20 @@
+print('Tuplas')
+#Inmutables
+
+semestre_1 = (3264, 1325, 2254, 533, 6954, 7784)
+
+#Tupla tiene que llevar , después del elemento
+es_tupla = ('Emtech',)
+print(es_tupla)
+
+#Convertirla en tupla
+tupla_vacia = tuple()
+
+enero = ('Computadoras', 1850)
+print("Productos:", enero[0])
+print("Total exportaciones: ", enero[1])
+
+#Agregar
+
+semestre_1 = semestre_1 + (860,230)
+print(semestre_1)
